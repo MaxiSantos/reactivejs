170735f8859e1f2b432ab92a8a1593ad13a676cb
maxi <rpenaranda@belatrixsf.com>
2016-10-31 18:22:36 -0300

    Adding observable creator

--- src-server/example_01_observable.js
+++ src-server/example_01_observable.js
@@ -4,9 +4,7 @@ import Rx from "rxjs/Rx";
 //   console.log("In promise");
 //   resolve("hey");
 // })
-
 // promise.then(item => console.log(item));
-
 
 const simple$ = new Rx.Observable(observer => {
   console.log("Generating observable");
@@ -23,23 +21,53 @@ const error$ = new Rx.Observable(observer => {
   observer.error(new Error("Ops !"));
 })
 
-//simple$.subscribe(
-error$.subscribe(
-  item => console.log(`one.next ${item}`),
-  error => console.log(`one.error ${error}`),
-  () => console.log(`one.complete`)
-);
+// simple$.subscribe(
+// //error$.subscribe(
+//   item => console.log(`one.next ${item}`),
+//   error => console.log(`one.error ${error}`),
+//   () => console.log(`one.complete`)
+// );
 
 // creating a second subscription
+// setTimeout(() => {
+//   simple$.subscribe({
+//     next: item => console.log(`two.next ${item}`), //this use current context with arrow key
+//     error(error){
+//       console.log(`two.error: ${error}`)
+//     },
+//     complete(){
+//       console.log("two.complete")
+//     }
+//   })
+// }, 3000);
+
+
+// Part 2
+function createSubscriber (tag) {
+  return {
+    next(item) { console.log(`${tag}.next ${item}`);},
+    error(error) { console.log(`${tag}.erro ${error.stack || error}`);},
+    complete() { console.log(`${tag}.complete`);}
+  }
+}
+
+function createInterval$(time) {
+  return new Rx.Observable(observer => {
+    let index = 0;
+    //let interval = setInterval(() => {
+    setInterval(() => {
+      observer.next(index++);
+    }, time);
+
+    // return () => {
+    //   clearInterval(interval);
+    // }
+  });
+}
+
+const everySecond$ = createInterval$(1000);
+const subscription = everySecond$.subscribe(createSubscriber("one"));
 
 setTimeout(() => {
-  simple$.subscribe({
-    next: item => console.log(`two.next ${item}`), //this use current context with arrow key
-    error(error){
-      console.log(`two.error: ${error}`)
-    },
-    complete(){
-      console.log("two.complete")
-    }
-  })
-}, 3000);
+  subscription.unsubscribe();
+}, 3500)
